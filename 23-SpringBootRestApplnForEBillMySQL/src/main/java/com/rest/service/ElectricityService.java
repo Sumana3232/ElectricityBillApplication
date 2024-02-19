d1b3b22b3e23e0132232269156f6c32af3495764
Sumana3232 <160462768+Sumana3232@users.noreply.github.com>
2024-02-19 12:23:13 +0530

    Add files via upload

---
+++ 23-SpringBootRestApplnForEBillMySQL/src/main/java/com/rest/service/ElectricityService.java
@@ -0,0 +1,15 @@
+package com.rest.service;
+
+import java.util.List;
+
+import com.rest.model.Electricity;
+
+public interface ElectricityService {
+
+	public Electricity saveRecord(Electricity electricity);
+	public Electricity updateRecord(Electricity electricity,int cid);
+	public void deleteRecord(int cid);
+	public Electricity getoneRecord(int cid);
+	public List<Electricity> getAllRecord();
+	
+}
